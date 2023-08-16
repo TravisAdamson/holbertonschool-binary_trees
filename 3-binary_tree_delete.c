b09c1ae744610f1df9a0d8136c418cc1aee6b9fd
Travis <6611@holbertonstudents.com>
2023-08-16 09:54:09 -0700

    Trying to fix invalid frees

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -14,8 +14,5 @@ void binary_tree_delete(binary_tree_t *tree)
 		binary_tree_delete(tree->left);
 	if (tree->right != NULL)
 		binary_tree_delete(tree->right);
-	free(tree->left);
-	free(tree->right);
-	if (tree != NULL)
-		free(tree);
+	free(tree);
 }
