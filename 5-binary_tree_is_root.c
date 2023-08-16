538ddac4494160225557093b2913dc2733ed4fe8
Travis <6611@holbertonstudents.com>
2023-08-16 10:09:38 -0700

    Added is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Verify if given node is a root
+ * @node: The node to examine
+ *
+ * Return: Returns 1 if root, otherwise 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	return (0);
+}
