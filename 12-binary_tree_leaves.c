2dcb74458fb81152d23068d25991d02e08a156ef
Travis <6611@holbertonstudents.com>
2023-08-17 09:28:06 -0700

    Fixed function name

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,7 +6,7 @@
  *
  * Return: The height of the tree, or 0 if NULL
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	int left_l = 0, right_l = 0;
 
