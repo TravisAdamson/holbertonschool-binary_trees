fecec4d90a96017a7af506e220b3db69bb686feb
Travis <6611@holbertonstudents.com>
2023-08-17 07:08:28 -0700

    Added some print statements

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_h = 0, right_h = 0, tree_h = 0;
+	size_t left_h = 0, right_h = 0, tree_h = 0;
 
 	if (tree != NULL)
 	{
 		left_h = binary_tree_height(tree->left);
 		right_h = binary_tree_height(tree->right);
 		tree_h += c_max(left_h, right_h);
+		printf("tree_h = %lu, left_h = %lu, right_h = %lu", tree_h, left_h, right_h);
 		return (tree_h);
 	}
 	else
