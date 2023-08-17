a7360710a0301247550052e1ea2991d6263c1985
Travis <6611@holbertonstudents.com>
2023-08-17 07:50:03 -0700

    Wrong if

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree != NULL)
 	{
 		return(1 + c_max(binary_tree_height(tree->left), binary_tree_height(tree->right)));
 	}
