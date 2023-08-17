900cbaa0bcf6f38a71f18bf9488ed08ccc832db4
Travis <6611@holbertonstudents.com>
2023-08-17 09:15:03 -0700

    Adding leaf checker

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Counts the number of leaves
+ * @tree: The tree to traverse
+ *
+ * Return: The height of the tree, or 0 if NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	int left_l = 0, right_l = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	left_l = binary_tree_leaves(tree->left);
+	right_l = binary_tree_leaves(tree->right);
+	return (left_l + right_l);
+}
