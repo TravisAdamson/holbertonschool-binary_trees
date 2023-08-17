347d378a4d650042203519a46f0b25b4010ca5f8
Travis <6611@holbertonstudents.com>
2023-08-17 08:37:31 -0700

    Flipped the conditional

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,10 +8,8 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		return (1 + binary_tree_depth(tree->parent));
-	}
-	else
+	if (!tree)
 		return (0);
+	else
+		return (1 + binary_tree_depth(tree->parent));
 }
