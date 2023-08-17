2317f9ff16e8f835f18fc5e9c07631bb2bf07610
Travis <6611@holbertonstudents.com>
2023-08-17 06:53:56 -0700

    Separating left and right calcs

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,9 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	int left_h = 0, right_h = 0;
+
 	if (tree != NULL)
 	{
-		return (1 + c_max(binary_tree_height(tree->left), binary_tree_height(tree->right)));
+		left_h = binary_tree_height(tree->left);
+		right_h = binary_tree_height(tree->right);
+		return (1 + c_max(left_h, right_h));
 	}
 	else
 		return (0);
