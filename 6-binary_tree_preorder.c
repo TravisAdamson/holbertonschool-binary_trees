13fa36ffba392b899b08c7b5b46c0318fb8bf80e
Travis <6611@holbertonstudents.com>
2023-08-16 17:05:17 -0700

    First preorder push

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Uses preorder to traverse given tree
+ * @tree: The tree to traverse
+ * @func: Function pointer
+ *
+ * Return: No return value
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL)
+	{
+		func(tree->data);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
