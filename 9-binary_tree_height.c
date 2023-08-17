5c4f599e33933896da5bb363d825760b20fcbbaa
Travis <6611@holbertonstudents.com>
2023-08-17 06:44:36 -0700

    Removed unusued variable and added a )

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,9 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
-
 	if (tree != NULL)
 	{
-		return (1 + c_max(binary_tree_height(tree->left), binary_tree_height(tree->right));
+		return (1 + c_max(binary_tree_height(tree->left), binary_tree_height(tree->right)));
 	}
 	else
 		return (0);
