85c6c760d6a93fb3e8e1727b806157dcd0b0ab94
Travis <6611@holbertonstudents.com>
2023-08-17 09:50:35 -0700

    Added balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Finds the balance of the given tree
+ * @tree: The tree to traverse
+ *
+ * Return: The balance of the tree, or 0 if NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_h = 0, right_h = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	if (tree->left != NULL)
+		left_h = binary_tree_balance(tree->left);
+	if (tree->right != NULL)
+		right_h = binary_tree_balance(tree->right);
+	return (left_h - right_h);
+}
