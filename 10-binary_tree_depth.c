07edad3108f0c564c3fa560fe311f1826cb001de
Travis <6611@holbertonstudents.com>
2023-08-17 08:46:34 -0700

    Fixing betty issues

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,11 +10,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	else
-	{
-		if (tree->parent != NULL)
-			return (1 + binary_tree_depth(tree->parent));
-		else
-			return (0);
-	}
+	if (tree->parent != NULL)
+		return (1 + binary_tree_depth(tree->parent));
+	return (0);
 }
