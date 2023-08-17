90dbcf9d71800dcb4178429707c9a85083d329a1
Travis <6611@holbertonstudents.com>
2023-08-17 08:32:18 -0700

    Added depth finder

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Gets the depth of the given tree
+ * @tree: The tree to traverse
+ *
+ * Return: The height of the tree, or 0 if NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		return (1 + binary_tree_depth(tree->parent));
+	}
+	else
+		return (0);
+}
