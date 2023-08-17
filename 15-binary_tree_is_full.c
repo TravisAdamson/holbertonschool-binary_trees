36c7f650fea482e3cfb6eb0ee90703214ba5410e
Travis <6611@holbertonstudents.com>
2023-08-17 11:20:33 -0700

    Adding full check

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+
+/**
+ * binary_tree_is_full - Checs if the tree is full
+ * @tree: The tree to traverse
+ *
+ * Return: The balance of the tree, or 0 if NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_balance(tree->left) && binary_tree_balance(tree->right));
+	return (1);
+}
