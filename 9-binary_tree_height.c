d286a75bc7f43995cb085a8bc16eecfac96e5488
Travis <6611@holbertonstudents.com>
2023-08-17 06:04:43 -0700

    Adding height check

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Gets the height of the given tree
+ * @tree: The tree to traverse
+ *
+ * Return: The height of the tree, or 0 if NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height = 0;
+
+	if (tree != NULL)
+	{
+		height = 1 + max(binary_tree_height(tree->left), binary_tree_height(tree->right));
+		return (height);
+	}
+	else
+		return (0);
+}
