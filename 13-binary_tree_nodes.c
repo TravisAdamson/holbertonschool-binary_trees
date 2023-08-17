c780358cdf03c9c716cef09f3329224582f343bb
Travis <6611@holbertonstudents.com>
2023-08-17 09:40:47 -0700

    Still 1 off

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,10 +13,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 	if (tree->left != NULL)
 		left_l = binary_tree_nodes(tree->left);
 	if (tree->right != NULL)
 		right_l = binary_tree_nodes(tree->right);
-	return (left_l + right_l);
+	return (left_l + right_l + 1);
 }
