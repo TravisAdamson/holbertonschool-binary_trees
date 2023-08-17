d0769acafaad13a21fdd06f7441732892b582df6
Travis <6611@holbertonstudents.com>
2023-08-17 08:18:00 -0700

    More order changes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,15 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_h = 0, right_h = 0;
+	
 	if (tree != NULL)
 	{
-		if (tree->left)
-			left_h = binary_tree_height(tree->left);
 		if (tree->right)
 			right_h = binary_tree_height(tree->right);
-		return(1 + c_max(right_h, left_h));
+		if (tree->left)
+			left_h = binary_tree_height(tree->left);
+		return (1 + c_max(right_h, left_h));
 	}
 	else
-		return(0);
+		return (0);
 }
