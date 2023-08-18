7137d95edf51832c95103e0b2cd24439fe562897
Travis <6611@holbertonstudents.com>
2023-08-17 17:25:23 -0700

    Adding uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+#include "17-binary_tree_sibling.c"
+
+/**
+ * binary_tree_uncle - Checs if the node has an uncle
+ * @node: The node sent to use to look for a sibling
+ *
+ * Return: A pointer to the sibling node, or NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+	return (binary_tree_sibling(node->parent))
+}
